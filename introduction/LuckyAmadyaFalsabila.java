1855befdd142d1791412b5924a729656ed64129f
1uckyamd <maelstromku@gmail.com>
2024-05-27 19:22:37 +0700

    2213010379{Lucky Amadya Falsabila}

---
+++ introduction/LuckyAmadyaFalsabila.java
@@ -0,0 +1,16 @@
+
+public class LuckyAmadyaFalsabila {
+
+    public static void main(String[] args) {
+        String fullname = "Lucky Amadya Falsabila";
+        String nim = "2213010379";
+        String team = "Team Underground";
+        String role = "Marketing";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
