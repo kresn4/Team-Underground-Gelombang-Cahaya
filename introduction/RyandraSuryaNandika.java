4b7a6536d1b1b1421206ac83826a630ae7a01a4b
ryandra344 <ryandrasuryanandika3@gmail.com>
2024-05-27 12:30:27 +0700

    2213010344 {Ryandra Surya Nandika}

---
+++ introduction/RyandraSuryaNandika.java
@@ -0,0 +1,16 @@
+
+public class RyandraSuryaNandika {
+
+    public static void main(String[] args) {
+        String fullname = "Ryandra Surya Nandika";
+        String nim = "2213010344";
+        String team = "Team Underground";
+        String role = "Leader";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
