d182fcbea15310fbdc56d8424a4138a1a75a485f
bittersweet <129123413+kresn4@users.noreply.github.com>
2024-05-28 14:37:15 +0700

    2213010380 {Kresna Bayu Prasetyo}

---
+++ introduction/KresnaBayuPrasetyo.java
@@ -0,0 +1,15 @@
+public class KresnaBayuPrasetyo {
+
+    public static void main(String[] args) {
+        String fullname = "Kresna Bayu Prasetyo";
+        String nim = "2213010380";
+        String team = "Team Underground";
+        String role = "Designer";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
